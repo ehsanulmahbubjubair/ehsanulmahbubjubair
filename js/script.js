62c35048f9d173db4daad947d8d2ebad08b87587
Ehsanul Mahbub Jubair <ehsanulmahbubjubair@gmail.com>
2024-04-30 02:01:36 +0600

    responsive laptop, tab, mobile

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
+/*======================== toggle icon navbar ========================*/
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
 /*======================== scroll sections avtive link ========================*/
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelectorAll('header nav a');
@@ -16,4 +25,12 @@ window.onscroll = () => {
             });
         };
     });
+    /*======================== sticky navber ========================*/
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    /*======================== remove toggle icon and navbar when click navber link (scroll) ========================*/
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
 };
